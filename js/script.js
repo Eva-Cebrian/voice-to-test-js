ad20ea32bab282850edd20ed48dc5ef3ed895c49
Eva Cebrian <65074646+Eva-Cebrian@users.noreply.github.com>
2024-10-27 16:25:45 +0100

    speech

--- js/script.js
+++ js/script.js
@@ -4,13 +4,23 @@ boton_convertir.addEventListener('click',function(){
     var speech = true;
     window.SpeechRecognition = window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
+    recognition.interimResults = true;  // actualizaciones en tiempo real mientras el usuario habla, en lugar de esperar hasta que termine de hablar para obtener el resultado final.
+
+    recognition.addEventListener('result', e=>{
+        const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+
+        convertir_texto.innerHTML = transcript;
+    })
 
     if (speech == true) {
         recognition.start();
     }
 
 })
+
+console.log("Esto es una prueba");
 
 
 
